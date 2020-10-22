86c4bc6e0e08db0e2b7b5e0b5d708a8340aa46f2
Aditya Hajare <aditya43@gmail.com>
2020-10-22 15:45:02 +0530

    GET Endpoint

--- 01-basics/src/products/products.controller.ts
+++ 01-basics/src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -10,5 +11,12 @@ export class ProductsController {
     @Get('/custom-get')
     customGet(): string {
         return 'Custom path GET route';
+    }
+
+    // http://localhost:3000/products/req-res?name=adi&city=pune
+    @Get('/req-res')
+    reqRes(@Req() req: Request, @Res() res: Response, @Query() query): void {
+        console.log(req);
+        res.send({ name: 'Aditya Hajare', query });
     }
 }
